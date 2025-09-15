7c67bdf5a8200870c696f652916a3ca4d1fdaef2
CraftedByMehathab <mehathab.shaik@techwavellc.io>
2025-09-15 14:02:01 -0400

    feat(page): spacing y

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { SkillsContainer } from "@/components/features/skills";
 
 export default function Home() {
   return (
-    <div className="px-4 md:px-8 lg:px-[10%] space-y-10">
+    <div className="px-4 md:px-8 lg:px-[10%] space-y-36">
       <HeroContainer />
       <SkillsContainer />
       <ContactContainer />
