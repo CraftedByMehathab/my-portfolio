0b5bfbf9c8780c5353dd75e55db4cb46c0f3b223
CraftedByMehathab <mehathab.shaik@techwavellc.io>
2025-09-16 12:41:16 -0400

    fix(terminal display): scroll auto

--- components/features/skills/skills-container.tsx
+++ components/features/skills/skills-container.tsx
@@ -72,7 +72,7 @@ export const SkillsContainer = () => {
             <SkillsRenderer skills={DEVOPS_SKILLSET} />
           </SkillSubContainer>
         </div>
-        <SkillSummary className="md:col-span-3 max-h-dvh overflow-y-scroll self-center" />
+        <SkillSummary className="md:col-span-3 max-h-dvh overflow-y-auto self-center" />
       </div>
     </ContainerLayout>
   );
