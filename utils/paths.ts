e98f10ccd3ede0d86e6989d2a00c87e5d5a01bb7
CraftedByMehathab <mehathab.shaik@techwavellc.io>
2025-10-12 15:45:12 -0400

    fix(resume): path

--- utils/paths.ts
+++ utils/paths.ts
@@ -3,5 +3,4 @@ export const contactPath = () => "/#contact";
 export const skillsPath = () => "/#skills";
 export const aboutPath = () => "/#about";
 export const projectsPath = () => "/#projects";
-export const resumePath = () =>
-  "/MehathabShaik_SeniorSoftwareEngineer_2025_.pdf";
+export const resumePath = () => "/MehathabShaik_Resume.pdf";
